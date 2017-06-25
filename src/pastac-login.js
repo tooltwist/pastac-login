a48921ddc80cb22e45a1392cf0c68ab9aefff6fe
philcal <philip.callender@tooltwist.com>
2017-06-26 06:27:13 +0800

    Changes during RatingSite work (modal template)
    - email login
    - oauth2 login
    - jwt
    - extra tests

--- src/pastac-login.js
+++ src/pastac-login.js
@@ -14,27 +14,31 @@ angular.module('pastac-login', [])
     hideForgot: '<', // boolean
     hideAvatar: '<', // boolean
     freshCredentials: '<', // boolean
+    registerWithPassword: '<', // boolean
+    facebook: '<', // boolean
+    google: '<', // boolean
+    github: '<', // boolean
     signin: '<', // boolean
     template: '<' // as-is string
   },
 
   templateUrl: function($element, $attrs) {
-    var t = '../dist/pastac-login-navbar.html';
+    var t = '/bower_components/pastac-login/dist/pastac-login-navbar.html';
     if ($attrs.template) {
       switch($attrs.template) {
         case 'navbar' :
           console.log('pastac-login: using navbar template.');
-          t = '../dist/pastac-login-navbar.html';
+          t = '/bower_components/pastac-login/dist/pastac-login-navbar.html';
           break;
 
         case 'modal' :
           console.log('pastac-login: using modal template.');
-          t = '../dist/pastac-login-modal.html';
+          t = '/bower_components/pastac-login/dist/pastac-login-modal.html';
           break;
 
         case 'page' :
           console.log('pastac-login: using page template.');
-          t = '../dist/pastac-login-page.html';
+          t = '/bower_components/pastac-login/dist/pastac-login-page.html';
           break;
 
         default:
@@ -57,16 +61,21 @@ function PastacLoginController($scope, $timeout) {
   var firstUser = true;
   var userHandler = null;
 
+  ctrl.junk = 'JUNKYARP'
+
   ctrl.$onInit = function() {
 
     // Set the initial mode
     ctrl.loggedIn = false;
+    ctrl.mode = 'login';
+    ctrl.setMode = function(mode) { ctrl.mode = mode; }
 
     userHandler = ctrl.handler;
 
 
     console.log('handler=', ctrl.handler);
     console.log('ctrl.freshCredentials=', ctrl.freshCredentials);
+    console.log('ctrl.registerWithPassword=', ctrl.registerWithPassword);
 
 
     // Get the user details.
@@ -75,6 +84,7 @@ function PastacLoginController($scope, $timeout) {
       port: AUTHSERVICE_PORT,
       tenant: AUTHSERVICE_TENANT,
       pretend: AUTHSERVICE_USE_DUMMY_LOGIN,
+      registerWithPassword: ctrl.registerWithPassword,
       onUserChange: function(user, ttuat, stale) {
 
         // If the current user came from the cookie, reload it
@@ -85,8 +95,9 @@ function PastacLoginController($scope, $timeout) {
         }
 
         // $timeout(function(){
-          ctrl.user = user;
-          console.log('\nUSER:', user);
+        //$scope.user = user;
+        ctrl.user = user;
+        console.log('\nSETTING USER:', user);
         // });
 
         // Notify the page using this component of the change in user status
@@ -99,16 +110,49 @@ function PastacLoginController($scope, $timeout) {
       }
     });
 
-    ctrl.initiate_oauth2 = function(authority) {
-
-      var url = 'http://' + AUTHSERVICE_HOST + ':' + AUTHSERVICE_PORT + '/v2/oauth2/initiate/' + AUTHSERVICE_TENANT + '/' + authority;
-      //alert('LOGIN using:' + url)
-      window.location = url;
+    ctrl.initiateOAuth2 = function(authority) {
+      authservice.initiateOAuth2(authority);
     }
 
 
+    ctrl.doEmailSignin = function() {
+      console.log('doEmailSignin(' + ctrl.username + ', ' + ctrl.password + ')');
+      var password = ctrl.password;
+      ctrl.password = '';
+      authservice.login(ctrl.username, password, function(user) {
+        //alert('got user!');
+        ctrl.username = '';
+        $('#pastac-login-modal').modal('hide')
+      }, function(error) {
+        //alert('Could not login: ' + error)
+        $timeout(function() {
+          ctrl.loginError = error;
+        }, 100);
+      })
+    }
+
+    ctrl.doLogout = function() {
+      //alert('ctrl.doLogout()')
+      authservice.logout();
+      ctrl.username = '';
+      ctrl.password = '';
+    }
+
+    ctrl.doRegister = function() {
+
+      //ZZZZ Need a spinner
+      var email = ctrl.register_email;
+      var password = ctrl.register_password;
+      ctrl.register_password = '';
 
 
+      console.log('doRegister(' + email + ', ' + email + ', ' + password + ')');
+      authservice.register(email, email, password, function(user) {
+        alert('You have been Registered. Check your email.')
+      }, function(error) {
+        alert('Could not login: ' + error)
+      })
+    }
 
 
   };//- onInit
